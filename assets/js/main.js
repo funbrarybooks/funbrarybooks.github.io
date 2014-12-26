e22855628f10af764e7ca41d43cee400128679dd
Ken Huang <ken52039@gmail.com>
2014-12-26 11:26:19 +0800

    更改oops翻譯

--- assets/js/main.js
+++ assets/js/main.js
@@ -147,5 +147,4 @@ $(document).on("submit", "form#SignInForm", function(event){
 function validateEmail(email) { 
 	var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 	return re.test(email);
-} 
-
+}
